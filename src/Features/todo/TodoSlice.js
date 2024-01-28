622791e31b1d77b0ca2e7d634b17f928ebd2a0bd
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-01-27 21:20:42 -0800

    TodoSlice

--- src/Features/todo/TodoSlice.js
+++ src/Features/todo/TodoSlice.js
@@ -5,14 +5,27 @@ export const initialState = {
 
     todos: [
         {
-            todoMsg : " heelo todo",
+            todoMsg: " heelo todo",
             id: 1,
         }
     ]
 }
 
-export const  todoSlice  = createSlice({
-    name : nanoid(),
+export const todoSlice = createSlice({
+    name: "todo",
     initialState,
-    r
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                todoMsg: action.payload,
+                id: nanoid()
+
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo)=> todo.id !== action.payload )
+
+         }
+    }
 })
